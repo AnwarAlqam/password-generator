93bf59c25ded29e7af1d6dbecda45ca6f4b9337d
Anwar Alqam <48700188+AnwarAlqam@users.noreply.github.com>
2024-05-18 04:46:07 +0000

    Added key icon, and slider

--- password-generator/src/app/app.component.ts
+++ password-generator/src/app/app.component.ts
@@ -1,21 +1,29 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 import { MatIconModule } from '@angular/material/icon';
 import { MatDividerModule } from '@angular/material/divider';
 import { MatButtonModule } from '@angular/material/button';
+import { MatSliderModule } from '@angular/material/slider';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, MatButtonModule, MatDividerModule, MatIconModule],
+  imports: [RouterOutlet, FormsModule, MatButtonModule, MatDividerModule, MatIconModule, MatSliderModule, MatInputModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'password-generator';
-  password = "Click the button to generate password :)";
-
-  number = 1;
+  password = "Click the button to generate a password :)";
+  disabled = false;
+  max = 32;
+  min = 0;
+  showTicks = false;
+  step = 1;
+  thumbLabel = false;
+  value = 0;
 
   ngOnInit() {
     // ...
